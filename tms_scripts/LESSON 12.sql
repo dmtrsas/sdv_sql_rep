42485a208251fd2a5cf1b5aaddad20e84cd87945
Dzmitry Sasnitski <dmitris3012@gmail.com>
2022-07-15 13:37:31 +0300

    delete files

--- tms_scripts/LESSON 12.sql
+++
@@ -1,9 +0,0 @@
-select count(distinct c.CompetitionID) as NumberofComp, count(r.ResultID), j.JudgeID, concat (j.FirstName, ' ', j.LastName)
-from People.Judge j
-join Competitions.Result r on r.JudgeID = j.JudgeID
-join Competitions.Competition c on c.CompetitionID = r.CompetitionID
-where YEAR(c.date_from) between '2018' and '2019'
-and YEAR(c.Date_to) between '2018' and '2019'
-group by j.JudgeID, concat (j.FirstName, ' ', j.LastName)
-having count(distinct c.CompetitionID) > 4
-order by count( c.CompetitionID) desc
